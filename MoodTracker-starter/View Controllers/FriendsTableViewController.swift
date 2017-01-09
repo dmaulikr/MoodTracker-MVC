f2cb1d01614824c86088e8331a2732114e7f9731
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-08 23:46:04 -0800

    started writing cellForRowAt function for TableViewController

--- MoodTracker-starter/View Controllers/FriendsTableViewController.swift
+++ MoodTracker-starter/View Controllers/FriendsTableViewController.swift
@@ -33,6 +33,10 @@ class FriendsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
     }
     
 }
