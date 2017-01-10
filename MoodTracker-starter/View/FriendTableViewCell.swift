d1fa7b824f28449dbcd633a88715b0d1f66f0fd8
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-09 23:23:07 -0800

    cleaned up code

--- MoodTracker-starter/View/FriendTableViewCell.swift
+++ MoodTracker-starter/View/FriendTableViewCell.swift
@@ -16,8 +16,13 @@ class FriendTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var moodButton: UIButton!
+    
     @IBAction func moodButtonPressed(_ sender: Any) {
+        
+        
         let newMood = friendsTableViewController.nextMood(mood: friend.mood) //go to next mood when button tapped
         friendsTableViewController.updateFriend(friend: friend, mood: newMood) //tell controller + update model/view
+        
+        
     }
 }
