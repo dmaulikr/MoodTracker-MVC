1a69d472867aefbec1d77ec09d16de6b29707ad3
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-11 23:53:17 -0800

    trying delegate/protocol communication between FriendsTableViewCell and FriendsTableViewController

--- MoodTracker-starter/AddFriendViewController.swift
+++ MoodTracker-starter/AddFriendViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-protocol AddFriendViewControllerDelegate {
-    func addFriend(f: Friend)
-}
+//protocol AddFriendViewControllerDelegate {
+//    func addFriend(f: Friend)
+//}
 
 class AddFriendViewController: UIViewController {
 
